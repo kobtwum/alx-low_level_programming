e16e1b4cd219de353a73ba722076ba3f9a4fa74c
kobtwum <kobinatwum@gmail.com>
2022-09-13 23:35:49 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet  in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (char letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+
+	return (0);
+}
+
